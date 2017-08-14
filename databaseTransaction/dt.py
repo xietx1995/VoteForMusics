8f44867a4a65fd3d66bc4b8b2885605383cd1f1e
Tianxin Tse <xietx1995@gmail.com>
2017-08-14 15:34:05 +0800

    增加修改数据库的函数

--- databaseTransaction/dt.py
+++ databaseTransaction/dt.py
@@ -7,6 +7,17 @@ dt.py
 import pymysql
 
 
+sentimens = {0: 's_happy',      1: 's_miss',         2: 's_terrified',
+             3: 's_troubled',   4: 's_disappointed', 5: 's_guilty',
+             6: 's_jealous',    7: 's_shy',          8: 's_wish',
+             9: 's_praise',     10: 's_angry',       11: 's_sacred',
+             12: 's_aroused',   13: 's_grand',       14: 's_solemn',
+             15: 's_peaceful',  16: 's_panic',       17: 's_hate',
+             18: 's_criticise', 19: 's_surprise',    20: 's_doubt',
+             21: 's_sad',       22: 's_warm',        23: 's_friendship',
+             24: 's_in_love',   25: 's_motivate',    26: 's_blue'}
+
+
 def connect_to_database(host, user, password, db, charset='utf8'):
     """
     连接mysql数据，并返回游标
@@ -34,3 +45,32 @@ def query(cursor, query_statement):
     result = cursor.fetchall()
 
     return result  # 返回值为一个元组，每个元素也为一个元组，包含了一条数据库记录
+
+
+def write(cursor, tb_name, entry_id, judge):
+    """
+    将用户评价写入数据库
+    :param cursor: 数据库游标
+    :param tb_name: 表名
+    :param entry_id: 记录的id
+    :param judge: 用户的评价
+    :return: None
+    """
+    # 从表单获得的是字符串值，需转换为整型
+    list_indices = []
+    for item in judge:
+        list_indices.append(int(item))
+
+    # 根据用户选择获得字段名
+    choices = []
+    for i in list_indices:
+        choices.append(sentimens[i])
+
+    # 构造sql语句
+    sql_statement = "UPDATE " + tb_name + " SET "
+    for s in choices:
+        sql_statement = sql_statement + s + "=" + s + "+1,"  # 例如：update tb_name set s1=s1+1,s2=s2+1...
+    sql_statement = sql_statement.rstrip(",")
+    sql_statement = sql_statement + " WHERE m_id=" + str(entry_id)
+    print(sql_statement)
+    cursor.execute(sql_statement)  # 执行sql语句
