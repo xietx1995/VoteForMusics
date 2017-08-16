3de694b0949b2ebf7e749975583f8db51b160055
Artprog <xietx1995@gmail.com>
2017-08-16 14:31:09 +0800

    add route /guide

--- app.py
+++ app.py
@@ -103,6 +103,11 @@ def finish():
     return render_template('finish.html')
 
 
+@app.route('/guide')
+def guide():
+    return render_template('guide.html')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
