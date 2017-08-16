0eb573dec4ee43c5b75ce48752093a9fb7ac9df5
Artprog <xietx1995@gmail.com>
2017-08-16 11:12:03 +0800

    change file encoding to utf8

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 from databaseTransaction import dt
+import sys
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
 class EssayForm(FlaskForm):
